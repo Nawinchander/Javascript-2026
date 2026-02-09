11214bce210ebc931380bde45c778c8edc2649aa
Nawin chander <nawinnapzy289@gmail.com>
2026-02-09 20:52:12 +0530

    add the files

---
+++ script.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log("Add:", num1 + num2);
+console.log("Subtract:", num1 - num2);
+console.log("Multiply:", num1 * num2);
+console.log("Divide:", num1 / num2);
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+function greet(name) {
+  console.log("Hello, " + name);
+}
+
+greet("Nawin");
